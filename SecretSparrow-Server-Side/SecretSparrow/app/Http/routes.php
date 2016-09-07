892fe88f7718d0920f8de1fc603b439926a01f33
william henry <william.hidayat@gmail.com>
2016-09-07 23:16:00 +1000

    Changing the framework version to Laravel 5.2 from Laravel 5.3. With some modifications for getFollowers and refreshStatus

---
+++ SecretSparrow-Server-Side/SecretSparrow/app/Http/routes.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('auth/twitter', 'TwitterAuthController@redirectToProvider');
+Route::get('auth/twitter/callback', 'TwitterAuthController@handleProviderCallback');
+
+Route::get('home', array('as' => 'home', 'uses' => function(){
+  return view('home');
+}));
+
+Route::get('api/requests/all','TwitterAPIController@refreshStatus');
+Route::get('api/requests/sent','TwitterAPIController@getFriendshipsStatus');
+Route::post('api/follow','TwitterAPIController@follow');//,'middleware'=>'auth:api']);
+Route::post('api/unfollow','TwitterAPIController@unfollow');//,'middleware'=>'auth:api']);
+Route::get('api/followers','TwitterAPIController@getFollowers');//,'middleware'=>'auth:api']);
+Route::get('api/friends','TwitterAPIController@getFollowing');
+Route::post('api/register','Auth\RegisterController@postRegister');
+//Route::get('api/access_token','Auth\RegisterController@getPersonalAccessToken');
---
+++ SecretSparrow-Server-Side/SecretSparrow/resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+                You're logged in
+            </div>
+            <h4>Your name is {{Auth::user()->name}}</h4>
+            <h4>Your Twitter handle is {{Auth::user()->handle}}</h4>
+            <img src="{{Auth::user()->avatar}}" height="200" width="200">
+        </div>
+    </body>
+</html>
